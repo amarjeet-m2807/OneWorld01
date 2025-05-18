bf18a6dd94e2e156f3af874a17e8fc56b5372694
ARYA <ARYA@DESKTOP-36Q0504>
2025-05-18 18:53:02 +0530

    Add Login

---
+++ Scripts/CRM/General/Login/Script1747574471417.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://patch.farvisioncloud.com/')
+
+WebUI.setText(findTestObject('Object Repository/CRM/General/Login/Page_Farvision/input_Welcome to Farvision_txtusername'), 
+    'uat42.crm')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/CRM/General/Login/Page_Farvision/input_Welcome to Farvision_txtpassword'), 
+    'ODh1n7B2jso5FK4NLIgviw==')
+
+WebUI.click(findTestObject('Object Repository/CRM/General/Login/Page_Farvision/button_Login'))
+
+WebUI.closeBrowser()
+
